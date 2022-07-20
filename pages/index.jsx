b960788b093341f68ef576951df05ff6f9601875
Muhammad Iqbal <iqbal.adudu@gmail.com>
2022-07-20 13:40:16 +0700

    product component

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ const Home = ({ products, bannerData}) => {
       </div>
 
       <div className="products-container">
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
       <FooterBanner />
